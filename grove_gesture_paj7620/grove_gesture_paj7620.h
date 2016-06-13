167c1662ccfe97748d5b1dbe5eaccbb3acf5204b
Jack Shao <jacky.shaoxg@gmail.com>
2016-06-13 16:29:55 +0800

    update gesture driver - replace tabs with spaces

--- grove_gesture_paj7620/grove_gesture_paj7620.h
+++ grove_gesture_paj7620/grove_gesture_paj7620.h
@@ -41,56 +41,56 @@
 //WIKI_URL          http://www.seeedstudio.com/wiki/Grove_-_Gesture_v1.0
 
 // REGISTER DESCRIPTION
-#define PAJ7620_VAL(val, maskbit)		( val << maskbit )
-#define PAJ7620_ADDR_BASE				0x00
+#define PAJ7620_VAL(val, maskbit)       ( val << maskbit )
+#define PAJ7620_ADDR_BASE               0x00
 
 // REGISTER BANK SELECT
-#define PAJ7620_REGITER_BANK_SEL		(PAJ7620_ADDR_BASE + 0xEF)	//W
+#define PAJ7620_REGITER_BANK_SEL        (PAJ7620_ADDR_BASE + 0xEF)  //W
 
 // DEVICE ID
 #define PAJ7620_ID  (0x73<<1)
 
 // REGISTER BANK 0
-#define PAJ7620_ADDR_SUSPEND_CMD		(PAJ7620_ADDR_BASE + 0x3)	//W
-#define PAJ7620_ADDR_GES_PS_DET_MASK_0		(PAJ7620_ADDR_BASE + 0x41)	//RW
-#define PAJ7620_ADDR_GES_PS_DET_MASK_1		(PAJ7620_ADDR_BASE + 0x42)	//RW
-#define PAJ7620_ADDR_GES_PS_DET_FLAG_0		(PAJ7620_ADDR_BASE + 0x43)	//R
-#define PAJ7620_ADDR_GES_PS_DET_FLAG_1		(PAJ7620_ADDR_BASE + 0x44)	//R
-#define PAJ7620_ADDR_STATE_INDICATOR	(PAJ7620_ADDR_BASE + 0x45)	//R
-#define PAJ7620_ADDR_PS_HIGH_THRESHOLD	(PAJ7620_ADDR_BASE + 0x69)	//RW
-#define PAJ7620_ADDR_PS_LOW_THRESHOLD	(PAJ7620_ADDR_BASE + 0x6A)	//RW
-#define PAJ7620_ADDR_PS_APPROACH_STATE	(PAJ7620_ADDR_BASE + 0x6B)	//R
-#define PAJ7620_ADDR_PS_RAW_DATA		(PAJ7620_ADDR_BASE + 0x6C)	//R
+#define PAJ7620_ADDR_SUSPEND_CMD            (PAJ7620_ADDR_BASE + 0x3)   //W
+#define PAJ7620_ADDR_GES_PS_DET_MASK_0      (PAJ7620_ADDR_BASE + 0x41)  //RW
+#define PAJ7620_ADDR_GES_PS_DET_MASK_1      (PAJ7620_ADDR_BASE + 0x42)  //RW
+#define PAJ7620_ADDR_GES_PS_DET_FLAG_0      (PAJ7620_ADDR_BASE + 0x43)  //R
+#define PAJ7620_ADDR_GES_PS_DET_FLAG_1      (PAJ7620_ADDR_BASE + 0x44)  //R
+#define PAJ7620_ADDR_STATE_INDICATOR        (PAJ7620_ADDR_BASE + 0x45)  //R
+#define PAJ7620_ADDR_PS_HIGH_THRESHOLD      (PAJ7620_ADDR_BASE + 0x69)  //RW
+#define PAJ7620_ADDR_PS_LOW_THRESHOLD       (PAJ7620_ADDR_BASE + 0x6A)  //RW
+#define PAJ7620_ADDR_PS_APPROACH_STATE      (PAJ7620_ADDR_BASE + 0x6B)  //R
+#define PAJ7620_ADDR_PS_RAW_DATA            (PAJ7620_ADDR_BASE + 0x6C)  //R
 
 // REGISTER BANK 1
-#define PAJ7620_ADDR_PS_GAIN			(PAJ7620_ADDR_BASE + 0x44)	//RW
-#define PAJ7620_ADDR_IDLE_S1_STEP_0		(PAJ7620_ADDR_BASE + 0x67)	//RW
-#define PAJ7620_ADDR_IDLE_S1_STEP_1		(PAJ7620_ADDR_BASE + 0x68)	//RW
-#define PAJ7620_ADDR_IDLE_S2_STEP_0		(PAJ7620_ADDR_BASE + 0x69)	//RW
-#define PAJ7620_ADDR_IDLE_S2_STEP_1		(PAJ7620_ADDR_BASE + 0x6A)	//RW
-#define PAJ7620_ADDR_OP_TO_S1_STEP_0	(PAJ7620_ADDR_BASE + 0x6B)	//RW
-#define PAJ7620_ADDR_OP_TO_S1_STEP_1	(PAJ7620_ADDR_BASE + 0x6C)	//RW
-#define PAJ7620_ADDR_OP_TO_S2_STEP_0	(PAJ7620_ADDR_BASE + 0x6D)	//RW
-#define PAJ7620_ADDR_OP_TO_S2_STEP_1	(PAJ7620_ADDR_BASE + 0x6E)	//RW
-#define PAJ7620_ADDR_OPERATION_ENABLE	(PAJ7620_ADDR_BASE + 0x72)	//RW
+#define PAJ7620_ADDR_PS_GAIN                (PAJ7620_ADDR_BASE + 0x44)  //RW
+#define PAJ7620_ADDR_IDLE_S1_STEP_0         (PAJ7620_ADDR_BASE + 0x67)  //RW
+#define PAJ7620_ADDR_IDLE_S1_STEP_1         (PAJ7620_ADDR_BASE + 0x68)  //RW
+#define PAJ7620_ADDR_IDLE_S2_STEP_0         (PAJ7620_ADDR_BASE + 0x69)  //RW
+#define PAJ7620_ADDR_IDLE_S2_STEP_1         (PAJ7620_ADDR_BASE + 0x6A)  //RW
+#define PAJ7620_ADDR_OP_TO_S1_STEP_0        (PAJ7620_ADDR_BASE + 0x6B)  //RW
+#define PAJ7620_ADDR_OP_TO_S1_STEP_1        (PAJ7620_ADDR_BASE + 0x6C)  //RW
+#define PAJ7620_ADDR_OP_TO_S2_STEP_0        (PAJ7620_ADDR_BASE + 0x6D)  //RW
+#define PAJ7620_ADDR_OP_TO_S2_STEP_1        (PAJ7620_ADDR_BASE + 0x6E)  //RW
+#define PAJ7620_ADDR_OPERATION_ENABLE       (PAJ7620_ADDR_BASE + 0x72)  //RW
 
 // PAJ7620_REGITER_BANK_SEL
-#define PAJ7620_BANK0		PAJ7620_VAL(0,0)
-#define PAJ7620_BANK1	PAJ7620_VAL(1,0)
+#define PAJ7620_BANK0                       PAJ7620_VAL(0,0)
+#define PAJ7620_BANK1                       PAJ7620_VAL(1,0)
 
 // PAJ7620_ADDR_SUSPEND_CMD
-#define PAJ7620_I2C_WAKEUP	PAJ7620_VAL(1,0)
-#define PAJ7620_I2C_SUSPEND	PAJ7620_VAL(0,0)
+#define PAJ7620_I2C_WAKEUP                  PAJ7620_VAL(1,0)
+#define PAJ7620_I2C_SUSPEND                 PAJ7620_VAL(0,0)
 
 // PAJ7620_ADDR_OPERATION_ENABLE
-#define PAJ7620_ENABLE		PAJ7620_VAL(1,0)
-#define PAJ7620_DISABLE		PAJ7620_VAL(0,0)
+#define PAJ7620_ENABLE                      PAJ7620_VAL(1,0)
+#define PAJ7620_DISABLE                     PAJ7620_VAL(0,0)
 
 #define INIT_REG_ARRAY_SIZE (sizeof(init_register_array)/sizeof(init_register_array[0]))
 
 typedef enum {
-	BANK0 = 0,
-	BANK1,		
+    BANK0 = 0,
+    BANK1,
 } bank_e;
 
 typedef enum {
@@ -106,33 +106,54 @@ typedef enum {
     Gesture_Wave,
 } GESTURE_FLAGS;
 
-#define GES_RIGHT_FLAG				PAJ7620_VAL(1,0)
-#define GES_LEFT_FLAG				PAJ7620_VAL(1,1)
-#define GES_UP_FLAG					PAJ7620_VAL(1,2)
-#define GES_DOWN_FLAG				PAJ7620_VAL(1,3)
-#define GES_FORWARD_FLAG			PAJ7620_VAL(1,4)
-#define GES_BACKWARD_FLAG			PAJ7620_VAL(1,5)
-#define GES_CLOCKWISE_FLAG			PAJ7620_VAL(1,6)
-#define GES_COUNT_CLOCKWISE_FLAG	PAJ7620_VAL(1,7)
-#define GES_WAVE_FLAG				PAJ7620_VAL(1,0)
+#define GES_RIGHT_FLAG              PAJ7620_VAL(1,0)
+#define GES_LEFT_FLAG               PAJ7620_VAL(1,1)
+#define GES_UP_FLAG                 PAJ7620_VAL(1,2)
+#define GES_DOWN_FLAG               PAJ7620_VAL(1,3)
+#define GES_FORWARD_FLAG            PAJ7620_VAL(1,4)
+#define GES_BACKWARD_FLAG           PAJ7620_VAL(1,5)
+#define GES_CLOCKWISE_FLAG          PAJ7620_VAL(1,6)
+#define GES_COUNT_CLOCKWISE_FLAG    PAJ7620_VAL(1,7)
+#define GES_WAVE_FLAG               PAJ7620_VAL(1,0)
 
-#define GES_REACTION_TIME		500				// You can adjust the reaction time according to the actual circumstance.
-#define GES_ENTRY_TIME			800				// When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s). 
-#define GES_QUIT_TIME			1000
+#define GES_REACTION_TIME       500             // You can adjust the reaction time according to the actual circumstance.
+#define GES_ENTRY_TIME          800             // When you want to recognize the Forward/Backward gestures, your gestures' reaction time must less than GES_ENTRY_TIME(0.8s).
+#define GES_QUIT_TIME           1000
 
 
 class GroveGesture
 {
 public:
     GroveGesture(int pinsda, int pinscl);
+
+    /**
+     * Read the motion code. The upper direction is the one in which you read the silk-screen "U1".
+     *
+     * @param motion - 1-right, 2-left, 3-up, 4-down, 5-forward, 6-backward, 7-clockwise, 8-countclockwise, 255-sensor initialization fail
+     *
+     * @return bool
+     */
     bool read_motion(uint8_t *motion);
-private:
+
+    /**
+     * An event occurred when guesture detected.
+     */
+    DEFINE_EVENT(gesture, SULI_EDT_UINT8);
+
+
     I2C_T *i2c;
-    bool _init(void);
-    bool pajWakeUp();
-    void pajSelectBank(uint8_t bank);
-    bool pajWriteCmd(uint8_t addr, uint8_t cmd);
-    bool pajReadData(uint8_t addr, uint8_t qty, uint8_t data[]);
+    TIMER_T *timer;
+    bool isWaken;
+    bool new_data_available;
+    uint8_t cur_motion, last_motion;
+    bool _init(void) ICACHE_RAM_ATTR;
+    bool pajWakeUp()ICACHE_RAM_ATTR;
+    void pajSelectBank(uint8_t bank)ICACHE_RAM_ATTR;
+    bool pajWriteCmd(uint8_t addr, uint8_t cmd)ICACHE_RAM_ATTR;
+    bool pajReadData(uint8_t addr, uint8_t qty, uint8_t data[])ICACHE_RAM_ATTR;
+    void check_motion()ICACHE_RAM_ATTR;
 };
+
+static void grove_guesture_timer_interrupt_handler(void *para) ICACHE_RAM_ATTR;
 
 #endif
