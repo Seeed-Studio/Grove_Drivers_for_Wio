b9eb86b6046e72fee070efcba57dec6247d37b23
WarriorRocker <warriorrocker@gmail.com>
2016-07-06 19:15:41 -0400

    Fix description in grove_sound.h

--- grove_sound/grove_sound.h
+++ grove_sound/grove_sound.h
@@ -36,7 +36,7 @@
 //SKU               101020023
 //IF_TYPE           ANALOG
 //IMAGE_URL         http://www.seeedstudio.com/wiki/images/thumb/1/12/Sound_Sensor.jpg/400px-Sound_Sensor.jpg
-//DESCRIPTION       Grove - Sound Sensor can detect the sound strength of the environment. The main component of the module is a simple microphone, which is based on the LM358 amplifier and an electret microphone."
+//DESCRIPTION       "Grove - Sound Sensor can detect the sound strength of the environment. The main component of the module is a simple microphone, which is based on the LM358 amplifier and an electret microphone."
 //WIKI_URL          http://www.seeedstudio.com/wiki/Grove_-_Sound_Sensor
 //ADDED_AT          "2016-07-01"
 //AUTHOR            "SEEED"
