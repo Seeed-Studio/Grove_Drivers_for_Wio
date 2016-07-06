de0928417d70f69daad8633c377e95166133f7eb
WarriorRocker <warriorrocker@gmail.com>
2016-07-06 19:47:04 -0400

    Tabs to spaces for grove_luminance.cpp

--- grove_luminance/grove_luminance.cpp
+++ grove_luminance/grove_luminance.cpp
@@ -31,35 +31,35 @@
 
 GroveLuminance::GroveLuminance(int pin)
 {
-	this->io = (ANALOG_T *)malloc(sizeof(ANALOG_T));
-	suli_analog_init(io, pin);
+    this->io = (ANALOG_T *)malloc(sizeof(ANALOG_T));
+    suli_analog_init(io, pin);
 }
 
 bool GroveLuminance::read_luminance(float *lux)
 {
-	float vout_array[] = { 0.0011498, 0.0033908, 0.011498, 0.041803, 0.15199, 0.53367, 1.3689, 1.9068, 2.3 };
-	float  lux_array[] = { 1.0108,    3.1201,    9.8051,   27.43,    69.545,  232.67,  645.11, 73.52,  1000 };
+    float vout_array[] = { 0.0011498, 0.0033908, 0.011498, 0.041803, 0.15199, 0.53367, 1.3689, 1.9068, 2.3 };
+    float  lux_array[] = { 1.0108,    3.1201,    9.8051,   27.43,    69.545,  232.67,  645.11, 73.52,  1000 };
 
-	float vout = (suli_analog_read(io) * (3.0 / 1023.0));
-	*lux = _multi_map(vout, vout_array, lux_array, 9);
+    float vout = (suli_analog_read(io) * (3.0 / 1023.0));
+    *lux = _multi_map(vout, vout_array, lux_array, 9);
 
-	return true;
+    return true;
 }
 
 float GroveLuminance::_multi_map(float val, float * _in, float * _out, uint8_t size)
 {
-	// take care the value is within range
-	// val = constrain(val, _in[0], _in[size-1]);
-	if (val <= _in[0]) return _out[0];
-	if (val >= _in[size - 1]) return _out[size - 1];
+    // take care the value is within range
+    // val = constrain(val, _in[0], _in[size-1]);
+    if (val <= _in[0]) return _out[0];
+    if (val >= _in[size - 1]) return _out[size - 1];
 
-	// search right interval
-	uint8_t pos = 1;  // _in[0] allready tested
-	while (val > _in[pos]) pos++;
+    // search right interval
+    uint8_t pos = 1;  // _in[0] allready tested
+    while (val > _in[pos]) pos++;
 
-	// this will handle all exact "points" in the _in array
-	if (val == _in[pos]) return _out[pos];
+    // this will handle all exact "points" in the _in array
+    if (val == _in[pos]) return _out[pos];
 
-	// interpolate in the right segment for the rest
-	return (val - _in[pos - 1]) * (_out[pos] - _out[pos - 1]) / (_in[pos] - _in[pos - 1]) + _out[pos - 1];
+    // interpolate in the right segment for the rest
+    return (val - _in[pos - 1]) * (_out[pos] - _out[pos - 1]) / (_in[pos] - _in[pos - 1]) + _out[pos - 1];
 }
