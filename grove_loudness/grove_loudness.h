8a04f49b313e841deb9021e9cc429bb31fed9b35
WarriorRocker <warriorrocker@gmail.com>
2016-07-06 22:14:02 -0400

    Update author in grove_loudness.h

--- grove_loudness/grove_loudness.h
+++ grove_loudness/grove_loudness.h
@@ -39,7 +39,7 @@
 //DESCRIPTION       "The Grove - Loudness Sensor is designed to detect the loudness of environmental sound. Based on amplifier LM2904 and a built-in microphone, it amplifies and filters the high frequency signal that received from the microphone, and outputs a positive envelop."
 //WIKI_URL          http://www.seeedstudio.com/wiki/Grove_-_Loudness_Sensor
 //ADDED_AT          "2016-07-01"
-//AUTHOR            "SEEED"
+//AUTHOR            "Travis Brown"
 
 class GroveLoudness
 {
